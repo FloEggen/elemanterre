ef685eae2076e114220b422afa3d8be1a73599fb
Eggenschwiler Florent <f.eggenschwiler@itecor.com>
2025-10-20 20:51:05 +0200

    Updated RC date for 2025

--- router.js
+++ router.js
@@ -4,6 +4,6 @@ const userController = require('./controllers/userController')
 const objectController = require('./controllers/objectController')
 router.get('/', userController.home)
 router.get('/society', userController.society)
-router.get('/repair_coffee', userController.repairCoffee)
+router.get('/repair-coffee', userController.repairCoffee)
 router.post('/rc-object-registration', objectController.registerObject)
 module.exports = router
